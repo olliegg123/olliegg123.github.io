444f3b9d791e359efa89b56739d7ef029955adee
olliegg123 <olliegg123@gmail.com>
2020-10-06 23:15:17 +0100

    Updating spinning animation

--- games/script12.js
+++ games/script12.js
@@ -104,11 +104,11 @@ var cardList = [
   function flip(){
     var element = event.currentTarget;
     if (element.className === "card") {
-      if(element.style.transform == "rotateY(180deg)") {
-        element.style.transform = "rotateY(360deg)";
+      if(element.style.transform == "rotateY(-180deg)") {
+        element.style.transform = "rotateY(0deg)";
       }
       else {
-        element.style.transform = "rotateY(180deg)";
+        element.style.transform = "rotateY(-180deg)";
       }
     }
   };
